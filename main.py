90edf1974356cf8650201bc114426731d7f46340
jm <woalsdl500@naver.com>
2019-04-05 04:51:06 +0900

    UPDATE 주석

--- main.py
+++ main.py
@@ -98,8 +98,11 @@ class DetectionObject():
         self.ymax = int(self.ymin + h * h_scale)
         self.class_id = class_id
         self.confidence = confidence
-        
+
+# EntryIndex
+# location : output에서의 좌표
 def EntryIndex(side, lcoords, lclasses, location, entry):
+    # 몇번째 feature map
     n = int(location / (side * side))
     loc = location % (side * side)
     return int(n * side * side * (lcoords + lclasses + 1) + entry * side * side + loc)
@@ -133,24 +136,35 @@ def ParseYOLOV3Output(blob, resized_im_h, resized_im_w, original_im_h, original_
             anchor_offset = 2 * 3
         elif side == yolo_scale_52:
             anchor_offset = 2 * 0
+    # 26 * 26 / 13 * 13
+    side_square = side * side
 
-    side_square = side * side # 26 * 26 / 13 * 13
-    output_blob = blob.flatten() # flatten : 일렬
-    
+    # flatten : 일렬로 나열
+    # 256 * 256
+    # 256 * 169
+    output_blob = blob.flatten()
+
     for i in range(side_square): 
+        # 모든 좌표
         row = int(i / side) # i / 26 
         col = int(i % side) # i % 26
         for n in range(num):
-            obj_index = EntryIndex(side, coords, classes, n * side * side + i, coords) # 26,4,80,3*26*26+i,4
-            box_index = EntryIndex(side, coords, classes, n * side * side + i, 0) # 26,4,80,3*26*26+i,0
+            # object detection
+            obj_index = EntryIndex(side, coords, classes, n * side * side + i, coords)
+            box_index = EntryIndex(side, coords, classes, n * side * side + i, 0)
             scale = output_blob[obj_index]
             print(scale)
+            # threshold보다 작으면 skip
             if (scale < threshold):
                 continue
+            # boxing x,y,w,h
             x = (col + output_blob[box_index + 0 * side_square]) / side * resized_im_w
             y = (row + output_blob[box_index + 1 * side_square]) / side * resized_im_h
             height = math.exp(output_blob[box_index + 3 * side_square]) * anchors[anchor_offset + 2 * n + 1]
             width = math.exp(output_blob[box_index + 2 * side_square]) * anchors[anchor_offset + 2 * n]
+            ####################
+
+            # classification
             for j in range(classes):
                 class_index = EntryIndex(side, coords, classes, n * side_square + i, coords + 1 + j)
                 prob = scale * output_blob[class_index]
@@ -160,8 +174,11 @@ def ParseYOLOV3Output(blob, resized_im_h, resized_im_w, original_im_h, original_
                 # DetectionObject의 객체
                 obj = DetectionObject(x, y, height, width, j, prob, (original_im_h / resized_im_h), (original_im_w / resized_im_w))
                 objects.append(obj)
+            #####################
+
     return objects
 
+# box가 얼마나 겹쳐져있는지 확인
 def IntersectionOverUnion(box_1, box_2):
     width_of_overlap_area = min(box_1.xmax, box_2.xmax) - max(box_1.xmin, box_2.xmin)
     height_of_overlap_area = min(box_1.ymax, box_2.ymax) - max(box_1.ymin, box_2.ymin)
@@ -179,6 +196,7 @@ def IntersectionOverUnion(box_1, box_2):
     else:
         retval = (area_of_overlap / area_of_union)
     return retval
+
 
 # network 생성
 net = IENetwork(model = xml_path,weights = bin_path)
@@ -201,6 +219,7 @@ while(True) :
   prepimg = preprocess(frame,image_size,new_w,new_h)
 
   start = time.time()
+  # output : feature maps 확률
   outputs = exec_net.infer({'inputs': prepimg}) # inference
   end = time.time()
 
@@ -208,11 +227,17 @@ while(True) :
   
   objects = []
 
-  # outputs parsing
+  ########################
+  # parsing
+  # output : (1,256,13,13)
+  # output : (1,256,26,26)
+  ########################
   for output in outputs.values():
+      # object : DetectionObject 타입
       objects = ParseYOLOV3Output(output, new_h, new_w, cam_h, cam_w, 0.4, objects)
-  # object : DetectionObject 타입
+
   # Filtering overlapping boxes
+  # box를 걸러낸다
   objlen = len(objects)
   for i in range(objlen):
       # 신뢰도 == 0 skip
@@ -220,12 +245,16 @@ while(True) :
           continue
       
       for j in range(i + 1, objlen):
+          # box가 많이 겹쳐져있다면 그중에 신뢰도가 높은 box를 뽑아 사용
           if (IntersectionOverUnion(objects[i], objects[j]) >= 0.4):
               if objects[i].confidence < objects[j].confidence:
                   objects[i], objects[j] = objects[j], objects[i]
               objects[j].confidence = 0.0
-  
+  #############################
+
+
   # Drawing boxes
+  # 박스 그리기
   for obj in objects:
       # 정확도가 20% 미만 skip
       if obj.confidence < 0.2:
@@ -248,8 +277,6 @@ while(True) :
       break
   elapsedTime = time.time() - t1
   fps = "(Playback) {:.1f} FPS".format(1/elapsedTime)
-  
-  # print(fps)
 
 cam.release()
 cv2.destroyAllWindows()
