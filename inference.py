a65d7b021ee733ba7299cc719bc74409c218f90c
jjeamin <woalsdl500@naver.com>
2019-09-21 14:44:23 +0100

    Update people counting main

---
+++ inference.py
@@ -0,0 +1,134 @@
+import cv2
+import math
+import numpy as np
+
+def preprocess(frame,image_size,new_w,new_h):
+	resized_image = cv2.resize(frame, (new_w, new_h), interpolation=cv2.INTER_CUBIC)
+
+	# 128로 채운다
+	canvas = np.full((image_size, image_size, 3), 128)
+	canvas[(image_size - new_h) // 2:(image_size - new_h) // 2 + new_h, (image_size - new_w) // 2:(image_size - new_w) // 2 + new_w, :] = resized_image
+
+	prepimg = canvas
+
+	prepimg = prepimg[np.newaxis, :, :, :]  # Batch size axis add
+	prepimg = prepimg.transpose((0, 3, 1, 2))  # NHWC to NCHW
+
+	return resized_image, prepimg
+
+# detected object location
+class DetectionObject():
+    xmin = 0
+    ymin = 0
+    xmax = 0
+    ymax = 0
+    class_id = 0
+    confidence = 0.0
+
+    def __init__(self, x, y, h, w, class_id, confidence, h_scale, w_scale):
+        self.xmin = int((x - w / 2) * w_scale)
+        self.ymin = int((y - h / 2) * h_scale)
+        self.xmax = int(self.xmin + w * w_scale)
+        self.ymax = int(self.ymin + h * h_scale)
+        self.class_id = class_id
+        self.confidence = confidence
+
+# EntryIndex
+# location : output에서의 좌표
+def EntryIndex(side, lcoords, lclasses, location, entry):
+    # 몇번째 feature map
+    n = int(location / (side * side))
+    loc = location % (side * side)
+    return int(n * side * side * (lcoords + lclasses + 1) + entry * side * side + loc)
+
+# inference output parsing *************
+def ParseYOLOV3Output(blob, resized_im_h, resized_im_w, original_im_h, original_im_w, threshold, objects, classes, coords,num):
+    anchors = [10,13,16,30,33,23,30,61,62,45,59,119,116,90,156,198,373,326]
+    
+    out_blob_h = blob.shape[2] # 26 13
+    out_blob_w = blob.shape[3] # 26 13
+
+    side = out_blob_h
+    anchor_offset = 0
+
+    if len(anchors) == 18:   ## YoloV3
+        if side == 13:
+            anchor_offset = 2 * 6
+        elif side == 26:
+            anchor_offset = 2 * 3
+        elif side == 52:
+            anchor_offset = 2 * 0
+
+    elif len(anchors) == 12: ## tiny-YoloV3 *********
+        if side == 13:
+            anchor_offset = 2 * 3
+        elif side == 26:
+            anchor_offset = 2 * 0
+
+    else:                    ## ???
+        if side == 13:
+            anchor_offset = 2 * 6
+        elif side == 26:
+            anchor_offset = 2 * 3
+        elif side == 52:
+            anchor_offset = 2 * 0
+    # 26 * 26 / 13 * 13
+    side_square = side * side
+
+    # flatten : 일렬로 나열
+    # 256 * 256
+    # 256 * 169
+    output_blob = blob.flatten()
+
+    for i in range(side_square): 
+        # 모든 좌표
+        row = int(i / side) # i / 26 
+        col = int(i % side) # i % 26
+        for n in range(num):
+            # object detection
+            obj_index = EntryIndex(side, coords, classes, n * side * side + i, coords)
+            box_index = EntryIndex(side, coords, classes, n * side * side + i, 0)
+            scale = output_blob[obj_index]
+            # print(scale)
+            # threshold보다 작으면 skip
+            if (scale < threshold):
+                continue
+            # boxing x,y,w,h
+            x = (col + output_blob[box_index + 0 * side_square]) / side * resized_im_w
+            y = (row + output_blob[box_index + 1 * side_square]) / side * resized_im_h
+            height = math.exp(output_blob[box_index + 3 * side_square]) * anchors[anchor_offset + 2 * n + 1]
+            width = math.exp(output_blob[box_index + 2 * side_square]) * anchors[anchor_offset + 2 * n]
+            ####################
+
+            # classification
+            for j in range(classes):
+                class_index = EntryIndex(side, coords, classes, n * side_square + i, coords + 1 + j)
+                prob = scale * output_blob[class_index]
+                # threshold보다 작으면 skip
+                if prob < threshold:
+                    continue
+                # DetectionObject의 객체
+                obj = DetectionObject(x, y, height, width, j, prob, (original_im_h / resized_im_h), (original_im_w / resized_im_w))
+                objects.append(obj)
+            #####################
+
+    return objects
+
+# box가 얼마나 겹쳐져있는지 확인 IOU
+def IntersectionOverUnion(box_1, box_2):
+    width_of_overlap_area = min(box_1.xmax, box_2.xmax) - max(box_1.xmin, box_2.xmin)
+    height_of_overlap_area = min(box_1.ymax, box_2.ymax) - max(box_1.ymin, box_2.ymin)
+    area_of_overlap = 0.0
+    if (width_of_overlap_area < 0.0 or height_of_overlap_area < 0.0):
+        area_of_overlap = 0.0
+    else:
+        area_of_overlap = width_of_overlap_area * height_of_overlap_area
+    box_1_area = (box_1.ymax - box_1.ymin)  * (box_1.xmax - box_1.xmin)
+    box_2_area = (box_2.ymax - box_2.ymin)  * (box_2.xmax - box_2.xmin)
+    area_of_union = box_1_area + box_2_area - area_of_overlap
+    retval = 0.0
+    if area_of_union <= 0.0:
+        retval = 0.0
+    else:
+        retval = (area_of_overlap / area_of_union)
+    return retval
---
+++ main.py
@@ -0,0 +1,152 @@
+try:
+    from armv7l.openvino.inference_engine import IENetwork, IEPlugin
+except:
+    from openvino.inference_engine import IENetwork, IEPlugin
+
+import cv2
+import imutils
+import time
+import os
+import schedule
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from inference import *
+
+LABELS = ("person", "bicycle", "car", "motorbike", "aeroplane",
+      "bus", "train", "truck", "boat", "traffic light",
+      "fire hydrant", "stop sign", "parking meter", "bench", "bird",
+      "cat", "dog", "horse", "sheep", "cow",
+      "elephant", "bear", "zebra", "giraffe", "backpack",
+      "umbrella", "handbag", "tie", "suitcase", "frisbee",
+      "skis", "snowboard", "sports ball", "kite", "baseball bat",
+      "baseball glove", "skateboard", "surfboard","tennis racket", "bottle",
+      "wine glass", "cup", "fork", "knife", "spoon",
+      "bowl", "banana", "apple", "sandwich", "orange",
+      "broccoli", "carrot", "hot dog", "pizza", "donut",
+      "cake", "chair", "sofa", "pottedplant", "bed",
+      "diningtable", "toilet", "tvmonitor", "laptop", "mouse",
+      "remote", "keyboard", "cell phone", "microwave", "oven",
+      "toaster", "sink", "refrigerator", "book", "clock",
+      "vase", "scissors", "teddy bear", "hair drier", "toothbrush")
+
+# model setting
+classes = 80
+coords = 4
+num = 3
+
+image_size = 416
+cam_w = 416
+cam_h = 416
+
+new_w = int(cam_w * min(image_size/cam_w, image_size/cam_h))
+new_h = int(cam_h * min(image_size/cam_w, image_size/cam_h))
+
+xml_path = "./IR/yolo_v3.xml" #<--- MYRIAD
+bin_path = os.path.splitext(xml_path)[0] + ".bin"
+img_path = './test.jpg'
+
+# color setting
+label_text_color = (255, 255, 255)
+label_background_color = (125, 175, 75)
+box_color = (255, 128, 0)
+box_thickness = 1
+
+drawing = False
+
+# network 생성
+print("[INFO] Network generation")
+net = IENetwork(model = xml_path,weights = bin_path)
+print("[INFO] Success")
+
+# device (MYRIAD : NCS2)
+print("[INFO] Device Init")
+plugin = IEPlugin(device='MYRIAD')
+print("[INFO] Success")
+
+print("[INFO] Network Load...")
+exec_net = plugin.load(net)
+print("[INFO] Success")
+
+def filtering_box(objects):
+    # Filtering overlapping boxes
+    # box를 걸러낸다
+    objlen = len(objects)
+    for i in range(objlen):
+        # 신뢰도 == 0 skip
+        if (objects[i].confidence == 0.0):
+            continue
+        
+        for j in range(i + 1, objlen):
+            # box가 많이 겹쳐져있다면 그중에 신뢰도가 높은 box를 뽑아 사용
+            if (IntersectionOverUnion(objects[i], objects[j]) >= 0.4):
+                objects[j].confidence = 0.0
+                
+    return objects
+
+def object_counting(frame,objects, obj_label = 0, drawing=False):  
+    obj_count = 0
+  
+    # Drawing boxes
+    # 박스 그리기
+    for obj in objects:
+        if obj.confidence > 0.2:
+            label = obj.class_id
+            if label == obj_label:
+                obj_count += 1
+          
+            if drawing == True:
+                label_text = LABELS[label] + " (" + "{:.1f}".format(obj.confidence * 100) + "%)"
+                #frame boxing
+                cv2.rectangle(frame, (obj.xmin, obj.ymin), (obj.xmax, obj.ymax), box_color, box_thickness)
+                #frame label text
+                cv2.putText(frame, label_text, (obj.xmin, obj.ymin - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, label_text_color, 1)
+
+    return obj_count
+
+def parsing(outputs):
+    objects = []
+    
+    for output in outputs.values():
+        objects = ParseYOLOV3Output(output, new_h, new_w, cam_h, cam_w, 0.5, objects, classes, coords, num)
+
+    return filtering_box(objects)
+
+def job():    
+    start = time.time()
+    frame = cv2.imread(img_path)
+    
+    resized_frame, prepimg = preprocess(frame,image_size,new_w,new_h)
+
+    outputs = exec_net.infer({'inputs': prepimg}) # inference
+    
+    objects = parsing(outputs)
+    
+    count = object_counting(resized_frame, objects, obj_label = 0, drawing = drawing)
+    
+    print(count)
+    
+    end = time.time()
+  
+    print("1 epoch time : ", end - start)
+    
+    if drawing == True:
+        cv2.imshow('image',resized_frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+def main():
+    schedule.every(10).seconds.do(job)
+    
+    while True:
+        schedule.run_pending()
+    
+    del net
+    del exec_net
+    del plugin
+
+
+if __name__ == "__main__":
+    main()
+
+
+
